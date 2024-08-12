1877f35298363948ff8e2aa13b680aaf11e4500b
mvencel14 <85483029+mvencel14@users.noreply.github.com>
2024-08-12 20:23:24 +0200

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pacman -S xorg xorg-server xorg-xinit sddm plasma plasma-workspace plasma-wayland-protocols kwalletmanager konsole filelight ark gwenview kate okular spectacle dolphin kcalc discover packagekit-qt5 firefox dmidecode libreoffice phonon-qt5-vlc qbittorrent kdenlive
+pacman -S xorg xorg-server xorg-xinit sddm plasma plasma-workspace plasma-wayland-protocols kwalletmanager konsole filelight ark gwenview kate okular spectacle dolphin kcalc discover packagekit-qt6 firefox dmidecode libreoffice phonon-qt5-vlc qbittorrent kdenlive
 echo '[X11]' >> /etc/sddm.conf
 echo 'ServerArguments=-nolisten tcp -dpi 97' >> /etc/sddm.conf
